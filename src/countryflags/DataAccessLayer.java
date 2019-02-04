e613ec7f6301b4f9d0d5574136f9c8c19a6a8b0f
davem <dave.mike.ball@gmail.com>
2019-02-04 11:35:45 +0000

    Added  insert country

--- src/countryflags/DataAccessLayer.java
+++ src/countryflags/DataAccessLayer.java
@@ -51,7 +51,7 @@ public class DataAccessLayer {
          
     }
     
-     static String GetCapital( int ID) throws SQLException{
+    static String GetCapital( String Country) throws SQLException{
          
         
         try 
@@ -71,8 +71,80 @@ public class DataAccessLayer {
             {
                 Connection con = DriverManager.getConnection ("jdbc:ucanaccess://.\\src\\data\\countries1.accdb"); 
                 Statement stmt = (Statement) con.createStatement();
-           rs = stmt.executeQuery ("SELECT Capital FROM country where ID = '"+ ID+ "'");
-                 return rs.getString("Capital");
+                String Query = "SELECT Capital FROM country where Country = '"+ Country  + "'" ;
+                 System.out.println(Query);
+                rs = stmt.executeQuery (Query);
+                while(rs.next()){
+                return rs.getString("Capital");
+                }
+                return "Error";
+            }
+         catch(SQLException e)
+            {
+                System.out.println("SQL exception occured" + e);
+                 throw e;
+            }
+    //==========================================================='   
+         
+    }
+    
+    static ResultSet ReloadCountries() throws SQLException{
+         
+        
+        try 
+            {
+                Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+            } 
+        catch(ClassNotFoundException e) 
+            {
+                System.out.println("Class not found "+ e);
+            }
+
+         System.out.println("JDBC Class found");
+         
+          ResultSet rs;
+    //==========================================================='    
+         try 
+            {
+                Connection con = DriverManager.getConnection ("jdbc:ucanaccess://.\\src\\data\\countries1.accdb"); 
+                Statement stmt = (Statement) con.createStatement();
+           rs = stmt.executeQuery ("SELECT * FROM country");
+                 return rs;
+            }
+         catch(SQLException e)
+            {
+                System.out.println("SQL exception occured" + e);
+                 throw e;
+            }
+    //==========================================================='   
+         
+    }
+    
+
+     static void AddCountry( String Country, String Capital) throws SQLException{
+         
+        
+        try 
+            {
+                Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+            } 
+        catch(ClassNotFoundException e) 
+            {
+                System.out.println("Class not found "+ e);
+            }
+
+         System.out.println("JDBC Class found");
+         
+
+    //==========================================================='    
+         try 
+            {
+                Connection con = DriverManager.getConnection ("jdbc:ucanaccess://.\\src\\data\\countries1.accdb"); 
+                Statement stmt = (Statement) con.createStatement();
+                String Query = "INSERT INTO Country (Country  , Capital) VALUES ('"+ Country + "' , '"+ Capital+"') ";
+                 System.out.println(Query);
+                stmt.executeUpdate(Query);
+               
             }
          catch(SQLException e)
             {
